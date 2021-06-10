00189dec37b31190ee65e0b69e88b7429cfd8a9b
Roman <47779469+roman120487@users.noreply.github.com>
2021-06-10 21:27:19 +0300

    validator

--- src/app/pages/contact-us/contact-us.module.ts
+++ src/app/pages/contact-us/contact-us.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from 'src/app/shared/modules/shared.module';
 
 const routes: Routes = [{ path: '', component: ContactUsComponent }];
 
 @NgModule({
   declarations: [ContactUsComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), SharedModule],
 })
 export class ContactUsModule {}
--- src/app/pages/contact-us/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us/contact-us.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.scss']
+  styleUrls: ['./contact-us.component.scss'],
 })
 export class ContactUsComponent implements OnInit {
+  contactsForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private tostr: ToastrService) {}
 
   ngOnInit(): void {
+    this.reactiveForm();
   }
 
+  sendInfo() {
+    if (this.contactsForm.invalid) {
+      console.log('invalid');
+    } else {
+      this.tostr.success('data is send');
+    }
+  }
+
+  reactiveForm() {
+    this.contactsForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.email],
+      phone: ['+35480979455318', [Validators.required, this.validatorUAphone]],
+    });
+  }
+
+  validatorUAphone(control: AbstractControl) {
+    // const regExp = new RegExp('/^+380d{3}d{2}d{2}d{2}$/');
+    console.log(control);
+    console.log(control.value.match('[/^+380d{3}d{2}d{2}d{2}$/]'));
+
+    if (control.value.match('[/^+380d{3}d{2}d{2}d{2}$/]')) {
+      return null;
+    } else {
+      return { invalidPhone: true };
+    }
+  }
 }
